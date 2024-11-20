0404ad1f9bab839948019978f62cda52230e9365
Long Duong <hoanglong434gl@gmail.com>
2024-11-21 01:50:32 +0700

    feat:api auth

--- routes/customersRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const customerController = require('../controllers/customersController.js');
-
-router.get('/customers', customerController.getAllCustomers);
-router.get('/customers/:id', customerController.getCustomerById);
-router.post('/customers', customerController.addCustomer);
-
-module.exports = router;
