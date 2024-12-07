43d5b2ba3f7de39bd70ff5710c4fe17569a9767e
Long Duong <hoanglong434gl@gmail.com>
2024-12-08 00:27:54 +0700

    api get lytPoint

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getCustomerIds, getEmployeeList, getCustomerList, addNewEmployee, deleteEmployee, updateEmployeeInfo } = require('../controllers/userController');
+const { getCustomerIds, getEmployeeList, getCustomerList, addNewEmployee, deleteEmployee, updateEmployeeInfo, getLoyaltyPoint } = require('../controllers/userController');
 const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/employee', authMiddleware, getEmployeeList);
 router.get('/customer', authMiddleware, getCustomerList);
+router.get('/getloyaltyPnt', authMiddleware, getLoyaltyPoint);
 router.post('/addEmployee', authMiddleware, addNewEmployee);
 router.delete('/removeEmployee/:id', authMiddleware, deleteEmployee);
 router.put('/updateEInfo/:id', authMiddleware, updateEmployeeInfo);
