25748889dc735b4c08e0be60e3c2d5cb1d918da1
Long Duong <hoanglong434gl@gmail.com>
2024-12-08 12:56:35 +0700

    fix api user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCustomerIds, getEmployeeList, getCustomerList, addNewEmployee, deleteEmployee, updateEmployeeInfo, getLoyaltyPoint } = require('../controllers/userController');
+const { getCustomerIds, getEmployeeList, getCustomerList, addNewEmployee, deleteEmployee, updateEmployeeInfo, getLoyaltyPoint, getEmployeeById } = require('../controllers/userController');
 const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/employee', authMiddleware, getEmployeeList);
 router.get('/customer', authMiddleware, getCustomerList);
 router.get('/getloyaltyPnt', authMiddleware, getLoyaltyPoint);
+router.get('/getEmployee/:id', authMiddleware, getEmployeeById);
 router.post('/addEmployee', authMiddleware, addNewEmployee);
 router.delete('/removeEmployee/:id', authMiddleware, deleteEmployee);
 router.put('/updateEInfo/:id', authMiddleware, updateEmployeeInfo);
